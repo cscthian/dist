6d4f747d9185fd2c63ba36fb487cf60446b7ba57
csantacruz1127 <csantacruz1127@gmail.com>
2021-08-04 02:19:23 -0500

    inicio

---
+++ precache-manifest.45f6844921346c0bfac5d651edc53e4c.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fc6815263a921cd63ca81892a95cd75",
+    "url": "/img/f1.0fc68152.jpg"
+  },
+  {
+    "revision": "d966916ab88c6025ccc5f79333421361",
+    "url": "/logoDS.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "73198b579a08c7c1b8819d4c938ad042",
+    "url": "/img/gol2.73198b57.gif"
+  },
+  {
+    "revision": "fbfd88cc91bfcabb4443",
+    "url": "/js/chunk-vendors.5952f248.js"
+  },
+  {
+    "revision": "e412010338c1f17e2fc488f7a3bd9a70",
+    "url": "/img/fondoc.e4120103.jpg"
+  },
+  {
+    "revision": "3d1a27482a67fe5ea921bd13295c1bd1",
+    "url": "/img/c-icon4.3d1a2748.png"
+  },
+  {
+    "revision": "4318006ab5432b062bdd866c83205629",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8fd59676ac4dcbdbdcaab68e5c6ff46",
+    "url": "/img/te1.a8fd5967.jpg"
+  },
+  {
+    "revision": "5938f3e4236fd79fdacfcb69edd1b514",
+    "url": "/img/fondoO.5938f3e4.jpg"
+  },
+  {
+    "revision": "465b272a919bbe2dc007d7c9cbba681f",
+    "url": "/img/ds.465b272a.png"
+  },
+  {
+    "revision": "12918b135377cc375c99",
+    "url": "/js/app.8df1b8ab.js"
+  },
+  {
+    "revision": "88dbe1c621084f37698f37ce5caf74eb",
+    "url": "/img/logo2.88dbe1c6.jpg"
+  },
+  {
+    "revision": "bdcacc1bee57b5fa85e25e65937a02b5",
+    "url": "/img/logo5.bdcacc1b.jpg"
+  },
+  {
+    "revision": "23b21d6dcf8544d62dad0e593ebae968",
+    "url": "/img/logo1.23b21d6d.png"
+  },
+  {
+    "revision": "6999ef29f1bebfcb64baa96bf0ffba0a",
+    "url": "/img/pl1.6999ef29.png"
+  },
+  {
+    "revision": "73e4e312dee300d7e631957941793d70",
+    "url": "/img/pl2.73e4e312.png"
+  },
+  {
+    "revision": "4324a1044d9595bdc95a999e0cb15f52",
+    "url": "/img/c-icon2.4324a104.png"
+  },
+  {
+    "revision": "563261fb9185f9b87aee3725ef639c05",
+    "url": "/img/banner1.563261fb.jpg"
+  },
+  {
+    "revision": "bca3bf03acf822e03eb26eb958a67a33",
+    "url": "/img/avatar1.bca3bf03.png"
+  },
+  {
+    "revision": "fbfd88cc91bfcabb4443",
+    "url": "/css/chunk-vendors.de693cc0.css"
+  },
+  {
+    "revision": "12918b135377cc375c99",
+    "url": "/css/app.8b14777c.css"
+  }
+];
